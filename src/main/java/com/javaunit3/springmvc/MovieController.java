09e3eff28120bea496c0ac26f526c87f63690ddc
Charneaka Lewis <clewis20@ggc.edu>
2023-06-12 21:06:46 -0400

    Initial commit

---
+++ src/main/java/com/javaunit3/springmvc/MovieController.java
@@ -0,0 +1,38 @@
+package com.javaunit3.springmvc;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+public class MovieController {
+    @Autowired
+    private BestMovieService bestMovieService;
+
+    @RequestMapping("/")
+    public String getIndexPage(){
+        return "index";
+    }
+
+    @RequestMapping("/bestMovie")
+    public String getBestMoviePage(Model model) {
+        model.addAttribute("bestMovie", bestMovieService.getBestMovie().getTitle());
+        return "bestMovie";
+    }
+
+    @RequestMapping("/voteForBestMovieForm")
+    public String voteForBestMovieFormPage() {
+        return "voteForBestMovie";
+    }
+
+    @RequestMapping("/voteForBestMovie")
+    public String voteForBestMovie(HttpServletRequest request, Model model) {
+        String movieTitle = request.getParameter("movieTitle");
+        model.addAttribute("MovieVote", movieTitle);
+        return "voteForBestMovie";
+    }
+}

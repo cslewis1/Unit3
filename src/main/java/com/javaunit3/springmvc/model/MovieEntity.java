944197026a6bf5dc4cfeaf2d135c4d4cf2362de9
Charneaka Lewis <clewis20@ggc.edu>
2023-06-13 22:49:37 -0400

    Adding Unit 3 Projects 2-3

---
+++ src/main/java/com/javaunit3/springmvc/model/MovieEntity.java
@@ -0,0 +1,71 @@
+package com.javaunit3.springmvc.model;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "movies")
+public class MovieEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "movie_Id")
+    private Integer id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "maturity_Rating")
+    private String maturityRating;
+
+    @Column(name = "genre")
+    private String genre;
+
+    @OneToMany(cascade = CascadeType.ALL)
+    @JoinColumn(name = "movie_id")
+    private List<VoteEntity> vote;
+
+    public void addVote(VoteEntity vote)
+    {
+        this.vote.add(vote);
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getMaturityRating() {
+        return maturityRating;
+    }
+
+    public void setMaturityRating(String maturityRating) {
+        this.maturityRating = maturityRating;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public List<VoteEntity> getVote() {
+        return vote;
+    }
+
+    public void setVote(List<VoteEntity> vote) {
+        this.vote = vote;
+    }
+}
